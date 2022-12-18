98603d5c070f7c9b5a1f06cb446292b1bb45f9c0
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2022-12-18 14:21:25 -0300

    Adicionando Sass

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,4 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import { Inter } from '@next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
+import styles from '../styles/styles.module.scss';
 
 export default function Home() {
   return (
