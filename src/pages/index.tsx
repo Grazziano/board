59f392b4624643d4a7c35d978697a5b404139918
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2022-12-18 15:16:11 -0300

    Criando titulo dinamico

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
+import Head from 'next/head';
 import styles from '../styles/styles.module.scss';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Primeiro projeto com Next JS</h1>
-    </div>
+    <>
+      <Head>
+        <title>Board - Organizando suas tarefas.</title>
+      </Head>
+      <div>
+        <h1>Primeiro projeto com Next JS</h1>
+      </div>
+    </>
   );
 }
