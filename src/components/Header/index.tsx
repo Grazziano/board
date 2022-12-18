91386fa614afd0b9c13b9216a2874c59b7c2ce5e
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2022-12-18 16:23:24 -0300

    Criando Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+import styles from './styles.module.scss';
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Link href="/">
+          <img src="/images/logo.svg" alt="Logo Meu board" />
+        </Link>
+
+        <nav>
+          <Link href="/">Home</Link>
+          <Link href="/board">Meu board</Link>
+        </nav>
+
+        <button>Entrar com github</button>
+      </div>
+    </header>
+  );
+}
