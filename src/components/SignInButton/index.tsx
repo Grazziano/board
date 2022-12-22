fec6c09d0220132d483389ec4f9d83d89af41fb0
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2022-12-22 10:12:31 -0300

    Autenticação de usuários

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -2,16 +2,27 @@ import styles from './styles.module.scss';
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 
+import { signIn, signOut, useSession } from 'next-auth/react';
+
 export function SignInButton() {
-  const session = true;
+  const { data: session } = useSession();
 
   return session ? (
-    <button type="button" className={styles.signInButton} onClick={() => {}}>
-      <img src="http://github.com/grazziano.png" alt="Foto do usuário" />
-      Olá Grazziano <FiX color="#737380" className={styles.closeIcon} />
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
+      <img src={session.user?.image} alt="Foto do usuário" />
+      Olá {session.user?.name}{' '}
+      <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton} onClick={() => {}}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn('github')}
+    >
       <FaGithub color="#FFB800" /> Entrar com github
     </button>
   );
