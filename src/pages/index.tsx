075c03fd99b753da5fc38f89ad719a4c819d0435
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2022-12-24 23:19:53 -0300

    Página home estática

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import styles from '../styles/styles.module.scss';
 
@@ -26,3 +27,10 @@ export default function Home() {
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  return {
+    props: {},
+    revalidate: 60 * 60, // Atualiza a cada 60 minutos
+  };
+};
