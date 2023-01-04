12557d0a988aac897054142669808487a6b5831c
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2023-01-03 22:30:17 -0300

    Mostrando donaters

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,24 @@
+import { useState } from 'react';
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import styles from '../styles/styles.module.scss';
 
-export default function Home() {
+import firebase from '../services/firebaseConnection';
+
+type Data = {
+  id: string;
+  donate: boolean;
+  lastDonate: Date;
+  image: string;
+};
+
+interface HomeProps {
+  data: string;
+}
+
+export default function Home({ data }: HomeProps) {
+  const [donaters, setDonaters] = useState<Data[]>(JSON.parse(data));
+
   return (
     <>
       <Head>
@@ -20,8 +36,11 @@ export default function Home() {
           </p>
         </section>
 
+        {donaters.length !== 0 && <h3>Apoiadores:</h3>}
         <div className={styles.donaters}>
-          <img src="http://github.com/grazziano.png" alt="Usuário 1" />
+          {donaters.map((item) => (
+            <img key={item.image} src={item.image} alt={item.image} />
+          ))}
         </div>
       </main>
     </>
@@ -29,8 +48,21 @@ export default function Home() {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
+  const donaters = await firebase.firestore().collection('users').get();
+
+  const data = JSON.stringify(
+    donaters.docs.map((u) => {
+      return {
+        id: u.id,
+        ...u.data(),
+      };
+    })
+  );
+
   return {
-    props: {},
+    props: {
+      data,
+    },
     revalidate: 60 * 60, // Atualiza a cada 60 minutos
   };
 };
